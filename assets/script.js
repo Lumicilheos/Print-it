c80ecc0aa5a4a4e423663e25df7bb05b568d4a54
Louis-Michel HALBWAX <l.halbwax@orange.fr>
2024-07-27 15:30:34 +0200

    Final Version

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,50 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let slideActuel = 0;
+
+const sliderImage = document.getElementById('sliderImage');
+const tagLine = document.getElementById('tagLine');
+const arrowLeft = document.getElementById('arrowLeft');
+const arrowRight = document.getElementById('arrowRight');
+const dots = document.querySelectorAll('.dot');
+
+// DOTS + UPTADE
+
+function updateSlide() {
+  const slider = slides[slideActuel];
+  sliderImage.setAttribute('src', 'assets/images/slideshow/' + slider.image);
+  tagLine.innerHTML = slider.tagLine;
+
+
+dots.forEach((dot, index) => {
+	if (index === slideActuel) {
+		dot.classList.add('dot_selected');
+	} else {
+		dot.classList.remove('dot_selected');
+	}
+});
+}
+
+
+// Arrow
+arrowRight.addEventListener("click", function () {
+  slideActuel++;
+  if (slideActuel >= slides.length) {
+    slideActuel = 0; 
+  }
+  updateSlide();
+});
+
+
+arrowLeft.addEventListener("click", function () {
+  slideActuel--;
+  if (slideActuel < 0) {
+    slideActuel = slides.length - 1; 
+  }
+  updateSlide();
+});
+
+updateSlide();
+
